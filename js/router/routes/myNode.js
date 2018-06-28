52a39a5a926408cc86963e6e532c94ba6a8027ac
jungsap <thomas.jung@sap.com>
2018-06-28 14:39:32 -0400

    Ex 4.1 Update 2018

---
+++ js/router/routes/myNode.js
@@ -0,0 +1,14 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0*/
+/*eslint-env node, es6 */
+"use strict";
+var express = require("express");
+
+module.exports = function() {
+	var app = express.Router();
+
+	app.get("/", (req, res) => {
+		res.type("text/plain").status(200).send("Hello World Node.js");
+	});
+
+	return app;
+};
